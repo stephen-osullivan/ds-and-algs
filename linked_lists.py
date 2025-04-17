adf1fc8d3ef6f201c352c610b64ebb1f247163d4
stephen-osullivan <31805109+stephen-osullivan@users.noreply.github.com>
2025-04-17 10:21:12 +0000

    finished linked lists

--- linked_lists.py
+++ linked_lists.py
@@ -16,43 +16,116 @@ class LinkedList:
     def __init__(self):
         # initialise an empty dummy head. This will point to our first node when we add it
         self.head = Node(None, None)
+        self.size = 0
+
+    def __str__(self):
+        return str(self.to_array())
+    
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, idx):
+        assert not self.is_empty(), 'List is empty'
+        self.validate_index(idx)
+        # 0th element
+        current_node = self.head.next
+        # increment if idx > 0
+        for i in range(idx):
+            current_node=current_node.next
+        return current_node.val
+
+    def get_size(self):
+        return self.size
+
+    def is_empty(self):
+        """Checks if the array is initialised"""
+        # if the head is pointing to None then we're empty
+        return self.head.next is None
+    
+    def validate_index(self, index: int):
+        """Checks whether the idx is in bounds"""
+        assert (0 <= index) and (index <= self.size), f'Index {index} is out of bounds. Current list size: {self.size}.' 
 
     def insert(self, val, index = None):
-        # start at head and traverse through list until we reach the end
+        """Inserts a value into the list. If index is None then put it at the end."""
+        # set index to end if empty
+        index = index or self.size
+        self.validate_index(index)
+        
         current_node = self.head
-        current_index = 0
-        # keep looping until either we run out of nodes or we hit the node before our index
-        while current_node.next and current_index < index:
+        for i in range(index):
             current_node = current_node.next
-            current_index +=1
-        if current_index < index:
-            # if we didn't reach our index then it is invalid
-            raise IndexError(f'Index {index} is out of bounds. Max index is {current_index}.')
-        else:
-            next_node = current_node.next
-            
-            # initialise node
-            new_node = Node(val, None)
-            current_node.next = new_node
+        next_node = current_node.next
+        
+        # add node
+        current_node.next = Node(val, next_node)
+        self.size +=1
 
-    def get_len(self):
-        len = 0
+    def pop(self, index = None):
+        """removes node at specified index from list and returns value. Return none if empty"""
+        if self.is_empty():
+            return None
+        
+        index = index or self.size -1
+        self.validate_index(index)
+        
         current_node = self.head
-        while current_node.next:
-            current_node=current_node.next
-            len += 1
-        return len
+        for i in range(index):
+            current_node = current_node.next
+        target_node = current_node.next
     
-    def pop(self, index):
-
-
+        # remove_node
+        current_node.next = target_node.next
+        self.size -=1
+        # return value
+        return target_node.val
+    
+    def search(self, val):
+        """search for a value. Return the idx if it exists, and None if not"""
+        if self.is_empty():
+            return None
+        
+        current_node = self.head
+        for i in range(self.size):
+            current_node=current_node.next
+            if current_node.val == val:
+                return i            
+        return None
+    
+    def to_array(self):
+        array = []
+        current_node = self.head
+        for i in range(self.size):
+            current_node = current_node.next
+            array.append(current_node.val)
+        return array
 
 if __name__ == '__main__':
-    def divide(a, b):
-        return a/b
+    print('Debug')
+    linked_list = LinkedList()
+    linked_list.insert(0)
+    linked_list.insert(1)
+    linked_list.insert(2)
+    print(linked_list)
+    linked_list.insert(6)    
+    print('insert 6 at end', linked_list)
+    linked_list.pop(1)    
+    print('pop first element', linked_list)
+    print('print elments', linked_list[0], linked_list[1], linked_list[2])
+    print('search for 6:', linked_list.search(6))
+    print('search for -1:', linked_list.search(-1))
+
+    # tests
+    print('Running Tests')
+    linked_list = LinkedList()
+    linked_list.insert(0)
+    linked_list.insert(1)
+    linked_list.insert(2)
     tests = [
-        {'input':{'a':1, 'b':2}, 'output':0.5},
-        {'input':{'a':1, 'b':2}, 'output':0.2},
-        {'input':{'a':1, 'b':0}, 'output':0.5}
+        (linked_list.get_size, {}, 3),
+        (linked_list.insert, {'val':5}, None),
+        (linked_list.get_size, {}, 4),
+        (linked_list.pop, {}, 5),
+        (linked_list.pop, {'index': 1}, 1),
     ]
-    run_tests(divide, tests)
+    run_tests(tests)
--- utils/testing.py
+++ utils/testing.py
@@ -1,19 +1,18 @@
 # some testing utilities
 
-def run_tests(fn, tests):
+def run_tests(tests):
+    """run a sequence of tests structured as a list of tuples: [(fn1, input_dict_1, output_1), etc. ]"""
     passed, failed = 0, 0
     for idx, test in enumerate(tests):
         try:
-            input = test['input']
-            output = fn(**input) 
-            if output == test['output']:
-                print(f'Test {idx} passed.')
+            fn, inputs, expected_output =  test
+            observed_output = fn(**inputs) 
+            if observed_output == expected_output:
                 passed +=1
             else:
                 print(f'Test {idx} failed.')
-                print(f'Inputs: {input}')
-                print(f'Observed Output: {output}')
-                print(f'Expected Output: {output}')
+                print(f'Inputs: {inputs}')
+                print(f"Expected Output: {expected_output}. Observed Output: {observed_output}.")
                 failed +=1
         except Exception as e:
             print(f'Test {idx} failed with following error:')
