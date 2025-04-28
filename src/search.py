eae3afd4c12494dcdc9952a7b853e9151525ac64
stephen-osullivan <31805109+stephen-osullivan@users.noreply.github.com>
2025-04-28 23:29:08 +0100

    Update search.py

--- src/search.py
+++ src/search.py
@@ -1,25 +1,21 @@
 #search.py
 
-def binary_search(val, array, verbose=False):
+def binary_search(self, target, nums) -> int:
     """performs binary search on a sorted array"""
     # time complexity O(log(n))
     # space complexity O(1)
-
-    if verbose:
-        print('Searching', array)
-
-    if len(array) == 0:
-        return False
+    def recursive_binary_search(left = 0, right = len(nums)-1):
+        mid = (left + right)//2
+        if left > right:
+            return -1
+        elif nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return binary_search(mid+1, right)
+        else:
+            return binary_search(left, mid-1)
+    return binary_search()
     
-    mid_idx = len(array)//2 # for length 2n or 2n+1 this gives position 
-    mid_val = array[mid_idx]
-    if mid_val == val:
-        return True
-    elif mid_val < val:
-        return binary_search(val, array[(mid_idx+1):])
-    else:
-        return binary_search(val, array[:mid_idx])
-
 if __name__ == "__main__":
     print(binary_search(10, []))
     print(binary_search(10, list(range(15))))
